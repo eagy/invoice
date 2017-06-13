3a0ec9c21aaf966823acf5d4abc56b7111fa2101
Daniel Eagy <daniel.eagy@gmail.com>
2017-06-13 07:21:20 -0700

    Added parameterized constructor

--- src/invoice/Invoice.java
+++ src/invoice/Invoice.java
@@ -6,10 +6,18 @@ import java.util.ArrayList;
 public class Invoice {
 	private BigDecimal total;
 	private ArrayList<LineItem> items; 
+	private int invoiceNo;
 	
 	public Invoice() {
 		total = new BigDecimal(0);
 		items = new ArrayList<>();
+		invoiceNo = 0;
+	}
+	
+	public Invoice(BigDecimal total, int invoiceNo) {
+		this.total = total;
+		this.items = new ArrayList<LineItem>();
+		this.invoiceNo = invoiceNo;		
 	}
 	
 	public void addItem(LineItem item) {
@@ -20,6 +28,14 @@ public class Invoice {
 	public void removeLineItem(int index) {
 		total = total.subtract(items.get(index).getLineItemAmount());
 		items.remove(index);
+	}
+	
+	public void setInvoiceNo(int invoiceNo) {
+		this.invoiceNo = invoiceNo;
+	}
+	
+	public int getInvoiceNo() {
+		return invoiceNo;
 	}
 	
 	public BigDecimal getTotal() {
